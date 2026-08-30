bcc55c6a62ae0768d550ce32b6934269ca10db8c
NicoleEvangelista12A <nicole.barross@eaportal.org>
2026-08-29 21:45:39 -0300

    arrumando tudo e iniciando o modo claro

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import './Hero.css';
 
-const Hero = () => {
-  return (
-    <section className="hero-section">
-      <div className="text-content">
-        <p className="sub-headline">HACKING THE CELL</p>
-        <h1 className="headline">Creating a<br />Biocomputer</h1>
-        <button className="cta-button">LEARN MORE</button>
-      </div>
-      
-      <div className="visual-element">
-        <div className="bio-art-shader"></div>
-      </div>
-      
-      <div className="pagination">
-        <span className="active">01</span> / 02
-      </div>
-    </section>
-  );
-};
+const botao = document.getElementById('menu-direito');
+const body = document.body;
 
-export default Hero;
+const temaSalvo = localStorage.getItem('tema');
+if (temaSalvo === 'dark') {
+  body.classList.add('dark-mode');
+}
+
+botao.addEventListener('click', () => {
+  body.classList.menu-direito('dark-mode');
+  
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('tema', 'dark');
+    botao.textContent = '☀️';
+  } else {
+    localStorage.setItem('tema', 'light');
+    botao.textContent = '🌙';
+  }
+});
+
+
+    const menu-direito = document.getElementById('menu-direito');
+    const navLinks = document.getElementById('navLinks');
+
+    menu-direito.addEventListener('click', () => {
+      navLinks.classList.menu-direito('active');
+      menu-direito.classList.menu-direito('active');
+    });
+
+     let slideIndex = 0;
+        const slides = document.querySelectorAll('.carousel-slide');
+        const indicators = document.querySelectorAll('.indicator');
+
+        function showSlide(index) {
+            if (index >= slides.length) slideIndex = 0;
+            if (index < 0) slideIndex = slides.length - 1;
+
+            slides.forEach((slide, i) => {
+                slide.style.transform = `translateX(-${slideIndex * 100}%)`;
+            });
+
+            indicators.forEach((ind, i) => {
+                ind.classList.menu-direito('active', i === slideIndex);
+            });
+        }
+
+        function moveSlide(direction) {
+            slideIndex += direction;
+            showSlide(slideIndex);
+        }
+
+        function currentSlide(index) {
+            slideIndex = index;
+            showSlide(slideIndex);
+        }
+
+
